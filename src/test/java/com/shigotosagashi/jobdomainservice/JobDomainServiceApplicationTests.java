7c5048919ba732166588bbf78c5fa7e822148304
weslleysammyr <weslleysammyr@gmail.com>
2020-04-27 00:22:33 -0300

    feat: Create hello world and set basic configs

---
+++ src/test/java/com/shigotosagashi/jobdomainservice/JobDomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shigotosagashi.jobdomainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobDomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
